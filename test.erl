739914f501eb0a83f89f4f663ba128429fed33a7
Ewa Kulesza <ewa.kulesza@ericsson.com>
2017-12-08 13:18:39 +0100

    Revertowanie

--- test.erl
+++
@@ -1,7 +0,0 @@
--module(test).
-
--compile(export_all).
-
-
-funkcja1(Argument) when is_list(Argument) ->
-	io:format("Commit 1 + ~w", [Argument]).
--- test2.erl
+++
@@ -1,8 +0,0 @@
--module(test2).
-
--compile(export_all).
-
-funkcja2(Arg1,Arg2) ->
-io:format("Przed stash: ~w,  ~w ~n", [Arg1,Arg2]).
-
-funkcja_do_usuniecia() -> nie_ok.
